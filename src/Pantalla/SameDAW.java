55722b9404a92ae01b8f9d9eb344992bace2bef0
Eloy <>
2019-02-25 12:07:18 +0100

    Primer paso

--- src/Pantalla/SameDAW.java
+++ src/Pantalla/SameDAW.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javafxapplication6;
+package Pantalla;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
