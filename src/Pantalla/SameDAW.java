fda3c1185cea2a630efefd48c9db7c57a1072472
PC06 <PC06@DESKTOP-2JMPCVA>
2019-04-09 09:58:07 +0200

    Pantalla responsiva con errores y comienzo de jugabilidad parte 2

--- src/Pantalla/SameDAW.java
+++ src/Pantalla/SameDAW.java
@@ -67,19 +67,104 @@ public class SameDAW extends Application {
         return matriz;
     }
     
-    public void mostrarMatriz(){
-        for (int y=0; y<=casillaX; y++){
-            for (int x=0; x<=casillaY; x++){
-                Circle circulo1 = new Circle(tamanoCasilla);
-                circulo1.setFill(Color.rgb(255,0,0,1.0));
-                circulo1.setLayoutX(x*tamanoCasilla/2);
-                circulo1.setLayoutY(y*tamanoCasilla/2);
+    public void creaRed(){
+        ArrayList<Line> listaLinea = new ArrayList();
+        for (int i = 0; i < finalCasillaX; i+= tamanoCasilla) {
+            line = new Line();
+            line.setStartX(i);
+            line.setEndX(i);
+            line.setStartY(0);
+            line.setEndY(finalCasillaY);
+            line.setStroke(Color.WHITE);            
+            listaLinea.add(line);
+            root.getChildren().add(line);
+        }
+        for (int i = 0; i < finalCasillaY; i+= tamanoCasilla) {
+            line2 = new Line();
+            line2.setStartX(0);
+            line2.setEndX(finalCasillaX);
+            line2.setStartY(i);
+            line2.setEndY(i);
+            line2.setStroke(Color.WHITE);
+            listaLinea.add(line2);
+            root.getChildren().add(line2);
+        }
+    }
+
+    public void mostrarCasillas(){
+        for (int y=0; y<=casillaX-1; y++){
+            for (int x=0; x<=casillaY-1; x++){
+               mostrarFichas(x,y);
             }
         }
     }
+    public void mostrarFichas(int x,int y){
+        switch(matriz.matrizTablero[x][y]){
+            case '0':
+                Circle circulo0 = new Circle(tamanoCasilla/2);
+                circulo0.setFill(Color.rgb(255,0,0,1.0));
+                // color rojo opaco
+                circulo0.setLayoutX(x*tamanoCasilla+tamanoCasilla/2);
+                circulo0.setLayoutY(y*tamanoCasilla+tamanoCasilla/2);
+                root.getChildren().add(circulo0);
+                break;
+            case 'a':
+                Circle circulo = new Circle(tamanoCasilla/2);
+                circulo.setFill(Color.rgb(255,0,0,1.0));
+                // color rojo opaco
+                circulo.setLayoutX(x*tamanoCasilla+tamanoCasilla/2);
+                circulo.setLayoutY(y*tamanoCasilla+tamanoCasilla/2);
+                root.getChildren().add(circulo);
+                break;
+            case 'b':
+                Circle circulo2 = new Circle(tamanoCasilla/2);
+                circulo2.setFill(Color.rgb(0,0,255,1.0));
+                // color azul opaco
+                circulo2.setLayoutX(x*tamanoCasilla+tamanoCasilla/2);
+                circulo2.setLayoutY(y*tamanoCasilla+tamanoCasilla/2);
+                root.getChildren().add(circulo2);
+                break;
+            case 'c':
+                Circle circulo3 = new Circle(tamanoCasilla/2);
+                circulo3.setFill(Color.rgb(0,255,0,1.0));
+                // color verde opaco
+                circulo3.setLayoutX(x*tamanoCasilla+tamanoCasilla/2);
+                circulo3.setLayoutY(y*tamanoCasilla+tamanoCasilla/2);
+                root.getChildren().add(circulo3);
+                break;
+            case 'd':
+                Circle circulo4 = new Circle(tamanoCasilla/2);
+                circulo4.setFill(Color.rgb(186,186,186,1.0));
+                // color gris opaco
+                circulo4.setLayoutX(x*tamanoCasilla+tamanoCasilla/2);
+                circulo4.setLayoutY(y*tamanoCasilla+tamanoCasilla/2);
+                root.getChildren().add(circulo4);
+                break;
+                
+            case 'e':
+                Circle circulo5 = new Circle(tamanoCasilla/2);
+                circulo5.setFill(Color.rgb(255,255,0,1.0));
+                // color amarillo opaco
+                circulo5.setLayoutX(x*tamanoCasilla+tamanoCasilla/2);
+                circulo5.setLayoutY(y*tamanoCasilla+tamanoCasilla/2);
+                root.getChildren().add(circulo5);
+                break;
+                
+            case 'f':
+                Circle circulo6 = new Circle(tamanoCasilla/2);
+                circulo6.setFill(Color.rgb(125,33,129,1.0));
+                // color purpura opaco
+                circulo6.setLayoutX(x*tamanoCasilla+tamanoCasilla/2);
+                circulo6.setLayoutY(y*tamanoCasilla+tamanoCasilla/2);
+                root.getChildren().add(circulo6);
+                break;
+        }
+    }
+
     public void start(Stage primaryStage) {
         
-        Pane root = new Pane();
+        matriz = new Matriz();
+        root = new Pane();
         BorderPane border = new BorderPane();
         primaryStage.setResizable(false);
         
@@ -90,18 +175,11 @@ public class SameDAW extends Application {
         menuItem1.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent event) {
-                
-                System.out.println("handle menuItem1");
-                
-                for (int i = 0; i < finalCasillaX; i+= tamanoCasilla) {
-                    root.getChildren().remove(line2);
-                }
-                
-                for (int i = 0; i < finalCasillaY; i+= tamanoCasilla) {
-                    root.getChildren().remove(line2);
-                }
-                
                 //Ventana extra emergente
+                if(imageView != null){
+                    imageView.setImage(null);
+                    root.getChildren().remove(imageView);
+                }
                 Pane root2 = new Pane();
                 Scene secondScene = new Scene(root2, 256, 512);
                 Stage newWindow = new Stage();
@@ -145,8 +223,8 @@ public class SameDAW extends Application {
                 Hbox1.setVisible(false);
                 //tamaño por defecto
                 System.out.println("Pequeño");
-                casillaX = 3;
-                casillaY = 4;
+                casillaX = 5;
+                casillaY = 5;
                 primaryStage.setWidth(dimensionX);
                 primaryStage.setHeight(dimensionY);
                 finalCasillaX = tamanoCasilla*casillaX;
@@ -161,8 +239,8 @@ public class SameDAW extends Application {
                             case 0:
                                 valor = 0;
                                 System.out.println("Pequeño");
-                                casillaX = 3;
-                                casillaY = 4;
+                                casillaX = 5;
+                                casillaY = 5;
                                 primaryStage.setWidth(dimensionX);
                                 primaryStage.setHeight(dimensionY);
                                 finalCasillaX = tamanoCasilla*casillaX;
@@ -216,14 +294,16 @@ public class SameDAW extends Application {
                         FileChooser fileChooser = new FileChooser();
                         fileChooser.setTitle("Seleccionar imagen");
 
-                        FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
-                        FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
-                        FileChooser.ExtensionFilter extFilterJPEG = new FileChooser.ExtensionFilter("JPEG files (*.jpeg)", "*.JPEG");
-                        FileChooser.ExtensionFilter extFilterGIF = new FileChooser.ExtensionFilter("GIF files (*.gif)", "*.GIF");
-                        fileChooser.getExtensionFilters().addAll(extFilterPNG);
-                        fileChooser.getExtensionFilters().addAll(extFilterJPG);
-                        fileChooser.getExtensionFilters().addAll(extFilterJPEG);
+                        FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG (*.png)", "*.PNG");
+                        FileChooser.ExtensionFilter extFilterBMP = new FileChooser.ExtensionFilter("Mapa de bits de 256 colores (*.bmp)", "*.BMP",".dib");
+                        FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG (*.jpg)", "*.JPG");
+                        FileChooser.ExtensionFilter extFilterJPEG = new FileChooser.ExtensionFilter("JPEG (*.jpeg)", "*.JPEG");
+                        FileChooser.ExtensionFilter extFilterGIF = new FileChooser.ExtensionFilter("GIF (*.gif)", "*.GIF");
+                        fileChooser.getExtensionFilters().addAll(extFilterBMP);
                         fileChooser.getExtensionFilters().addAll(extFilterGIF);
+                        fileChooser.getExtensionFilters().addAll(extFilterJPEG);
+                        fileChooser.getExtensionFilters().addAll(extFilterJPG);
+                        fileChooser.getExtensionFilters().addAll(extFilterPNG);
                         File file = fileChooser.showOpenDialog(null);
                         localUrl = null;
                         try {
@@ -252,32 +332,31 @@ public class SameDAW extends Application {
                             case 1:
                                 if (textField.getText().trim().isEmpty()) {
                                     System.out.println("Vacio1");
-                                    casillaX = 10;
-                                    finalCasillaX = tamanoCasilla*casillaX;
+                                    casillaY = 10;
+                                    finalCasillaY = tamanoCasilla*casillaY;
                                 }
                                 else if(textField.getText() !=null){
                                     System.out.println("Lleno1");
 
                                     int Inum = Integer.valueOf(textField.getText());
-                                    casillaX = Inum;
-                                    finalCasillaX = tamanoCasilla*casillaX;
+                                    casillaY = Inum;
+                                    finalCasillaY = tamanoCasilla*casillaY;
                                 }
                                 // Segundo textField
                                 if (textField2.getText().trim().isEmpty()) {
                                     System.out.println("Vacio2");
-                                    casillaY = 10;
-                                    finalCasillaY = tamanoCasilla*casillaY;
+                                    casillaX = 10;
+                                    finalCasillaX = tamanoCasilla*casillaX;
                                 }
                                 else if(textField2.getText() !=null){
                                     System.out.println("Lleno2");
                                     int Inum = Integer.valueOf(textField2.getText());
-                                    casillaY = Inum;
-                                    finalCasillaY = tamanoCasilla*casillaY;
-                                    
+                                    casillaX = Inum;
+                                    finalCasillaX = tamanoCasilla*casillaX;
                                 }
                             break;
                         }
-                        //
+                        //Añade el fondo
                         if (localUrl !=null){
                             Image image = new Image(localUrl);
                             imageView = new ImageView(image);
@@ -289,39 +368,24 @@ public class SameDAW extends Application {
                             root.getChildren().add(imageView);
                             System.out.println("imagen :D");
                         }
+                            
                         
                         //
                         dimensionX=finalCasillaX+3;
                         dimensionY=finalCasillaY+70;
-                        
+
+                        // crea una lista de lineas y las añade al root (panel principal y borde central)
                         if (cbl.isSelected()){
-                            ArrayList<Line> listaLinea = new ArrayList();
-                            for (int i = 0; i < finalCasillaX; i+= tamanoCasilla) {
-                                line = new Line();
-                                line.setStartX(i);
-                                line.setEndX(i);
-                                line.setStartY(0);
-                                line.setEndY(finalCasillaY);
-                                line.setStroke(Color.WHITE);            
-                                listaLinea.add(line);
-                                root.getChildren().add(line);
-                            }
-                            for (int i = 0; i < finalCasillaY; i+= tamanoCasilla) {
-                                line2 = new Line();
-                                line2.setStartX(0);
-                                line2.setEndX(finalCasillaX);
-                                line2.setStartY(i);
-                                line2.setEndY(i);
-                                line2.setStroke(Color.WHITE);
-                                listaLinea.add(line2);
-                                root.getChildren().add(line2);
-                            }
+                            creaRed();
                         }
+                        
+                        // Asignacion y aplicacion de tablero
+                        
+                        getTablero();
+                        mostrarCasillas();
+                        //
                         primaryStage.setWidth(dimensionX);
                         primaryStage.setHeight(dimensionY);
-                        // Asignacion y aplicacion de tablero
-                        getTablero();
-                        mostrarMatriz();
                         
                         newWindow.close();   
                     }
@@ -349,9 +413,8 @@ public class SameDAW extends Application {
         menuBar.setMinWidth(dimensionX);
         //menuItem
         // HBOX de abajo
-        Label label3 = new Label("Puntuacion:");
-        
         Label label9 = new Label(dato+" Bolas Seleccionadas("+multiplicador+" puntos)");
+        Label label3 = new Label("Puntuacion:");
         HBox hbox = new HBox(label9,label3);
         hbox.setSpacing(60);
         hbox.setMinHeight(10);
@@ -361,13 +424,13 @@ public class SameDAW extends Application {
         // HBOX de abajo
         
         //bordes
-        border.setTop(menuBar);
-        border.setCenter(root);
-        border.setBottom(hbox);
+        border.setTop(menuBar);// borde que contiene los menus
+        border.setCenter(root); // borde que contiene el panel principal
+        border.setBottom(hbox); // borde que contiene la puntuación
         
         //bordes
         
-
+        // parametros de los bordes
         Scene scene = new Scene(border,dimensionX, dimensionY,Color.web("#000000"));
         primaryStage.setWidth(dimensionX);
         primaryStage.setHeight(dimensionY);
